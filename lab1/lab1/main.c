c03fe01922e48453aa6d4d562bc17f67831a6a92
movieman12341 <movieman12341@yahoo.com>
2018-09-05 10:36:23 -0500

    lab 1 start

--- lab1/lab1/main.c
+++ lab1/lab1/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 struct node
 {
@@ -8,39 +9,96 @@ struct node
 };
 
 //function protos
-
-
+int valueSort(const void *a, const void *b);
+int removeDups(struct node * nArr, int nElements);
 
 int main()
 {
-	
-	struct node nodeArr[5000];
+	printf("How many elements do you want?\n");
+	int numElements;
+	scanf("%d", &numElements);
 
-	//input
-	int repeat = 1;
-	int index = 0;
-	while (repeat)
+	struct node *nodeArr = (struct node*) calloc(numElements, sizeof(struct node));
+
+	int i = 0;
+	for (; i < numElements; i++)
 	{
-		printf("enter an integer to put in array. Enter     -1     to stop inputting numbers\n");
+		printf("Enter an integer\n");
 		int num;
 		scanf("%d", &num);
-
-		if (num == -1)
-			repeat = 0;//stop loop
-		else
-		{
-			nodeArr[index].value = num;
-			nodeArr[index].position = index;
-			index++;
-		}
+		nodeArr[i].value = num;
+		nodeArr[i].position = i;
+	}
+	//initial value sort
+	qsort((void*)nodeArr, numElements, sizeof(nodeArr[0]), valueSort);
+	
+	//after sorting
+	printf("after sorting.\n\n");
+	for (i = 0; i < numElements; i++)
+	{
+		printf("%d %d\n", nodeArr[i].value, nodeArr[i].position);
 	}
 
-	//???????????????????????????????? 5000 ok?		can't find a way to make qsort stable.
-	//sort values
-	
+	//remove extras
+	removeDups(nodeArr, numElements);
+	printf("remove extras\n\n");
+	for (i = 0; i < numElements; i++)
+	{
+		printf("%d %d\n", nodeArr[i].value, nodeArr[i].position);
+	}
 
-	
 
+
+	free(nodeArr);
+	nodeArr = NULL;
 	return 0;
 }
 
+int valueSort(const void * a, const void * b)
+{
+	int l = ((struct node*) a)->value;
+	int r = ((struct node*) b)->value;
+	int value = l - r;
+
+	if (value == 0)//both a and b are equal
+	{
+		return ((struct node*) a)->position - ((struct node*) b)->position;
+	}
+	else
+		return value;
+}
+
+
+int removeDups(struct node * nArr, int nElements)
+{
+	/*
+	for i < nelements-1
+		cmp j to j+1 value
+		if != values
+			a[i++] = a[j++]
+
+		else if (== values
+			a[i] = 1st value of dup
+			
+	*/
+
+
+
+	int i = 0, j = 0;
+	for (; i < nElements - 1; i++)
+	{
+		if (nElements == 1 || nElements == 0)
+			return;
+
+
+		else if (nArr[j].value != nArr[j+1].value)
+		{
+			nArr[i] = nArr[j++];
+		}
+		else//== values
+		{
+			nArr[i].value = nArr[j++].value;
+		}
+	}
+	nArr[j++].value = nArr[nElements - 1].value;
+}
